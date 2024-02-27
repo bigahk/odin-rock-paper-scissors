c63c83f2f3d8e888e64add2480f9a5ee49134d1d
bigahk <nic.medina0424@gmail.com>
2024-02-27 13:41:58 -0500

    add user prompt and round function
    idk if i added user prompt in the last commit, but it prompts user to input a choice
    round uses if...else to specify what message should be displayed depending on whether it is a tie, loss, or win

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function getComputerChoice() {
 
     switch (randomNumber) {
         case 1:
-            return 'Rock';
+            return 'ROCK';
             break;
         case 2:
-            return 'Paper';
+            return 'PAPER';
             break;
         case 3:
-            return 'Scissors';
+            return 'SCISSORS';
             break;
     }
 }
@@ -24,9 +24,40 @@ function getComputerChoice() {
 //attach this response to a variable within the function
 //return a message correcting them if they do not put in some form of rock, paper, or scissors
 
-function playerSelection() {
+function getPlayerChoice() {
     userChoice = prompt('Please enter your choice');
     upperChoice = userChoice.toUpperCase();
+    return upperChoice
 }
 
 
+function playRound(computerSelection, playerSelection) {
+    if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' || playerSelection === 'PAPER' && computerSelection=== 'ROCK' || playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return(`You win! ${playerSelection} beats ${computerSelection}!`);
+    } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER' || playerSelection === 'PAPER' && computerSelection === 'SCISSORS' || playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+        return (`You lose! ${computerSelection} beats ${playerSelection}!`);
+    } else if (playerSelection === 'ROCK' && computerSelection === 'ROCK' || playerSelection === 'PAPER' && computerSelection === 'PAPER' || playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS') {
+        return (`It's a tie! Both players chose ${playerSelection}.`)
+    }
+    else {
+        return ('Your choice is unrecognized. Please enter Rock, Paper, or Scissors.');
+    }
+}
+
+//create a function that plays the user against the computer
+
+const computerSelection = getComputerChoice()
+const playerSelection = getPlayerChoice()
+
+//console.log(playRound(computerSelection, playerSelection))
+
+function playGame() {
+    let round = playRound(computerSelection, playerSelection)
+    
+    console.log(round);
+    console.log(round);
+    console.log(round);
+    console.log(round);
+    console.log(round);
+}
+console.log(playGame());
