274185f7a1b748b54affbbabf72ea7ed24c1b18d
bigahk <nic.medina0424@gmail.com>
2024-02-27 10:56:43 -0500

    add playerSelection function
    prompts user to input their choice
    converts their choice to all upper-case so that it does not matter what capitalization they use

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // create function getComputerChoice: randomly return Rock, Paper, or Scissors
 
 //need it to pick one of three options, array?
-//
+
 function getComputerChoice() {
 
     const randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -18,3 +18,15 @@ function getComputerChoice() {
             break;
     }
 }
+
+//must create a playerSelection function that accepts all forms of capitalization for their answer
+//prompt user for input (R,P,S)
+//attach this response to a variable within the function
+//return a message correcting them if they do not put in some form of rock, paper, or scissors
+
+function playerSelection() {
+    userChoice = prompt('Please enter your choice');
+    upperChoice = userChoice.toUpperCase();
+}
+
+
