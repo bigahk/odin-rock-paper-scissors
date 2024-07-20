fdbbe55fb6e350ed192dd6ce420e7422f8e22dad
bigahk <nic.medina0424@gmail.com>
2024-07-20 17:11:31 -0400

    Add very basic UI to RPS game Add button choices, score display sections Add click event listener to call gameplay function Change all console.logs to DOM methods

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// global variables for user score?
+// global variables for user score? --   this is fine
 let userScore = 0
 let computerScore = 0
 
-// generates computer choice randomly
+
+
+// generates computer choice randomly --   this is fine
 function getComputerChoice() {
 
     let randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -20,35 +22,54 @@ function getComputerChoice() {
     }
 }
 
-// prompts user for their choice
-function getPlayerChoice() {
-    let userChoice = prompt('Please enter your choice');
-    let upperChoice = userChoice.toUpperCase();
-    return upperChoice
-}
-
-// calls the above get functions to define variables for the next funciton
 
 
-// function to play a singular round, need to work on score keeping
+// Event listener calls on the playRound function and assigns value to computerSelection variable based on output of getComputerChoice
+let userChoice = document.querySelector('#container');
+userChoice.addEventListener('click', (event) => {
+    let target = event.target;
+    let playerSelection;
+    switch (target.id) {
+        case "rock":
+            playerSelection = "ROCK";
+            break;
+        case "paper":
+            playerSelection = "PAPER";
+            break;
+        case "scissor":
+            playerSelection = "SCISSORS";
+            break;
+
+    }
+    let computerSelection = getComputerChoice();
+
+
+    playRound(computerSelection, playerSelection);
+    document.querySelector('#total').textContent = `User Score: ${userScore}   Computer Score: ${computerScore}`; //fills in the total score div
+});
+
+
+
+// function to play a singular round, need to work on score keeping  -- modded to fill in div showing round score
+
 function playRound(computerSelection, playerSelection) {
+
     if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}!`)
+        document.querySelector('#round').textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
         return userScore++;
     } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER' || playerSelection === 'PAPER' && computerSelection === 'SCISSORS' || playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        console.log((`You lose! ${computerSelection} beats ${playerSelection}!`));
+        document.querySelector('#round').textContent = `You lose! ${computerSelection} beats ${playerSelection}!`;
         return computerScore++;
     } else if (playerSelection === 'ROCK' && computerSelection === 'ROCK' || playerSelection === 'PAPER' && computerSelection === 'PAPER' || playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS') {
-        console.log( `It's a tie! Both players chose ${playerSelection}.`);
-    }
-    else {
-        return ('Your choice is unrecognized. Please enter Rock, Paper, or Scissors.');
-    }
+        document.querySelector('#round').textContent = `It's a tie! Both players chose ${playerSelection}.`;
+    };
 }
 
-// helper functions, what do I need? if..else for when game is over to determine winner 
-function gameOver(){
-    if(computerScore > userScore) {
+
+
+// helper functions, what do I need? if..else for when game is over to determine winner <- unchanged and unused for this part
+function gameOver() {
+    if (computerScore > userScore) {
         console.log(`You lose! The computer won ${computerScore} rounds.`);
     } else if (userScore > computerScore) {
         console.log(`You win! You won ${userScore} rounds to the computer's ${computerScore}`);
@@ -62,29 +83,3 @@ function gameOver(){
 
 
 // 5 round game that keeps track of scores and declares winner at the end 
-function playGame() {
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
-    playRound(computerSelection, playerSelection);
-    console.log(`Your score is ${userScore}. The computer's score is ${computerScore}`);
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
-    playRound(computerSelection, playerSelection);
-    console.log(`Your score is ${userScore}. The computer's score is ${computerScore}`);
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
-    playRound(computerSelection, playerSelection);
-    console.log(`Your score is ${userScore}. The computer's score is ${computerScore}`);
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
-    playRound(computerSelection, playerSelection);
-    console.log(`Your score is ${userScore}. The computer's score is ${computerScore}`);
-    computerSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
-    playRound(computerSelection, playerSelection);
-    console.log(`Your score is ${userScore}. The computer's score is ${computerScore}`);
-    
-    gameOver();
-}
-
-playGame();
